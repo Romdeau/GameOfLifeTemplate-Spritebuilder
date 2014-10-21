d01fc224f73db27f53219c0782de07858ac99e0d
Thomas Taege <thomas.taege@gmail.com>
2014-10-21 19:35:36 +1000

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.m
@@ -0,0 +1,13 @@
+//
+//  Creature.m
+//  GameOfLife
+//
+//  Created by Thomas Taege on 21/10/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Thomas Taege on 21/10/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
